a294ef0ad9c078155eecaefcdb796c4361bac157
Vladimir <antonyanvladimir1992@gmail.com>
2020-03-02 23:12:32 +0100

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArmCooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
